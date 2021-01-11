4ac133e2433f27410679b2a48ab4d23988eee931
Michal Atlas <michal.z.atlas@gmail.com>
2021-01-11 16:13:35 +0100

    Tester doesn't halt on sandboxed error, TOS flag

--- tester.sh
+++ tester.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-#set -euo pipefail
-RUNC=$1;
+set -euo pipefail
 TDIR="tests";
+TEST_ONLY_SUCCESS=0;
+#COMPARE_ON_ERROR=0;
+ALL_TEST_PASS=1;
 
-for test_dir in $(find $TDIR -type d -not -name tests); do
+FLAG=1;
+while [ $FLAG -eq 1 ]
+do
+    case "$1" in
+        --test-only-success)
+            TEST_ONLY_SUCCESS=1;
+            shift;
+            ;;
+#        --compare-stdout-on-error)
+#            COMPARE_ON_ERROR=1;
+#            shift;
+#            ;;
+        *)
+            FLAG=0;
+            ;;
+    esac
+done
+
+for test_dir in "$TDIR"/*; do
     echo "[Running Job: $test_dir]";
 
     # EXECUTION
-    OG="$($RUNC < "$test_dir/in")";
-    ECG=$?; # Exit Code Got + Expected
+    OG="$("$@" < "$test_dir/in")" && true; # Exit Code Got + Expected
+    ECG="$?";
     ECE=$(cat "$test_dir/exit");
     OE=$(cat "$test_dir/out"); # Output Got + Expected
 
@@ -18,22 +38,28 @@ for test_dir in $(find $TDIR -type d -not -name tests); do
     then
         echo "OK";
     else
-        if [ "$OG" = "$OE" ]
-        then
-            echo "  OUTPUT: SUCCESS";
-        else
-            echo "  OUTPUT: FAILURE";
-            echo "      EXPECTED: $OE";
-            echo "      GOT: $OG";
-        fi;
-
-        if [ "$ECG" -eq "$ECE" ]
+        if [ $TEST_ONLY_SUCCESS -eq 1 ] && { { [ "$ECG" -eq 0 ] && [ "$ECE" -eq 0 ]; } || { [ ! "$ECG" -eq 0 ] && [ ! "$ECE" -eq 0 ]; }; } || [ "$ECG" -eq "$ECE" ]
         then
             echo "  EXIT CODE: SUCCESS";
         else
             echo "  EXIT CODE: FAILURE";
             echo "      EXPECTED: $ECE";
             echo "      GOT: $ECG";
+            ALL_TEST_PASS=0;
+        fi;
+
+        if [ ! $TEST_ONLY_SUCCESS ] && [ "$OG" = "$OE" ]
+        then
+            echo "  OUTPUT: SUCCESS";
+        else
+            echo "  OUTPUT: FAILURE";
+            echo "      EXPECTED: $OE";
+            echo "      GOT: $OG";
+            ALL_TEST_PASS=0;
         fi;
     fi;
 done;
+
+if [ $ALL_TEST_PASS -eq 1 ]; then
+    printf "\n--- ALL TESTS SUCCESSFULL ---\n";
+fi;
