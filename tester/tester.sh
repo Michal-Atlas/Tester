99be97bdb387ce2a657d4f7e6bacbf9cc6677d47
Michal Atlas <michal.z.atlas@gmail.com>
2021-02-22 19:15:27 +0100

    Added Summary to end

--- tester/tester.sh
+++ tester/tester.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-
+#!/bin/env bash
 set -uo pipefail
 
 testdir=$1;
@@ -9,6 +8,9 @@ runcmd=$1;
 if [[ $runcmd = "" ]]; then echo "Empty Runcommand"; exit 1; fi;
 shift;
 
+successfull="";
+failed="";
+
 for f in ./"$testdir"/*; do
     printf "%s: " "$(echo "$f" | sed "s/\.\/tests\///")";
     out=$($runcmd < "$f"/in) && true;
@@ -16,10 +18,16 @@ for f in ./"$testdir"/*; do
     if [[ "$excode" -eq 0 ]]; then
         if [[ "$out" = "$(cat "$f"/out)" ]]; then
             printf "OK\n";
+            successfull="$successfull $(echo "$f" | sed "s/\.\/tests\///")";
         else
             printf "\n\n___ DIFF ___\n%s\n___ END ___\n\n" "$(diff <(echo "$out") <(cat "$f"/out))";
+            failed="$failed $(echo "$f" | sed "s/\.\/tests\///")";
         fi;
+        
     else
         printf "[ Exit Code: %s ]\n" "$excode";
+        failed="$failed $(echo "$f" | sed "s/\.\/tests\///")";
     fi;
 done;
+
+printf "\n\nSuccessfull: %s\nFailed: %s\n" "$successfull" "$failed";
