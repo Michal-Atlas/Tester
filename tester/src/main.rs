e5bc4f015d649eb6d8ddd3a0fcf87c895f980f71
Michal Atlas <michal.z.atlas@gmail.com>
2021-03-17 20:48:41 +0100

    Added multithreading

--- tester/src/main.rs
+++ tester/src/main.rs
@@ -1,8 +1,9 @@
 use ansi_term::Colour;
 use std::env::args;
-use std::fs;
 use std::fs::{read_dir, File};
 use std::process::{Command, Output, Stdio};
+use std::{fs, thread};
+use std::time::SystemTime;
 
 #[derive(Debug)]
 struct Test {
@@ -22,81 +23,83 @@ fn main() {
             panic!("Problem reading directory: {}", e)
         }
     };
-    let mut failures: Vec<String> = vec![];
+    let mut handles = Vec::new();
     for test_dir in tests {
-        let test_dir = test_dir.unwrap();
-        let path = test_dir.path().clone();
-        println!(
-            "- {}:",
-            Colour::Cyan
-                .bold()
-                .paint(test_dir.file_name().to_str().unwrap())
-        );
-        let mut test = Test {
-            input: None,
-            output: "".to_string(),
-            exit_code: 0,
-        };
-        // Extract Test Info from Files
-        for f in path.read_dir().expect("Error reading TestDir") {
-            let f = match f {
-                Ok(r) => r,
-                Err(_) => {
-                    continue;
-                }
+        let run_cmd = run_cmd.clone();
+        let time_start = SystemTime::now();
+        handles.push(thread::spawn(move || {
+            let test_dir = test_dir.unwrap();
+            let path = test_dir.path().clone();
+            let mut test = Test {
+                input: None,
+                output: "".to_string(),
+                exit_code: 0,
             };
-            match f.file_name().to_str().unwrap() {
-                "in" => {
-                    test.input = Some(fs::File::open(f.path()).unwrap());
+            // Extract Test Info from Files
+            for f in path.read_dir().expect("Error reading TestDir") {
+                let f = match f {
+                    Ok(r) => r,
+                    Err(_) => {
+                        continue;
+                    }
+                };
+                match f.file_name().to_str().unwrap() {
+                    "in" => {
+                        test.input = Some(fs::File::open(f.path()).unwrap());
+                    }
+                    "out" => {
+                        test.output = fs::read_to_string(f.path()).unwrap();
+                    }
+                    "exit" => {
+                        test.exit_code = fs::read_to_string(f.path())
+                            .unwrap()
+                            .trim()
+                            .parse()
+                            .unwrap();
+                    }
+                    &_ => {}
                 }
-                "out" => {
-                    test.output = fs::read_to_string(f.path()).unwrap();
+            }
+            let output = Command::new(run_cmd)
+                .stdin(Stdio::from(test.input.unwrap()))
+                .output()
+                .expect("Failed to execute");
+            // Run the program
+
+            let out_string = match output {
+                Output { .. } if output.status.success() && test.exit_code != 0 => {
+                    "  - Succeed on supposed fail".to_string()
                 }
-                "exit" => {
-                    test.exit_code = fs::read_to_string(f.path())
+                Output { .. } if !output.status.success() && test.exit_code == 0 => {
+                    "  - Fail on supposed Success".to_string()
+                }
+                Output { .. }
+                    if std::str::from_utf8(output.stdout.as_slice())
                         .unwrap()
                         .trim()
-                        .parse()
-                        .unwrap();
+                        != test.output.trim() =>
+                {
+                    "  - Output not the same\n\n".to_string() +
+                    "diff:\n" +
+                    &Colour::Green.paint(fs::read_to_string(path.to_str().unwrap().to_owned() + "/out").unwrap()).to_string() +
+                        "\n" +
+                    &Colour::Red.paint(std::str::from_utf8(output.stdout.as_slice()).unwrap()).to_string()
                 }
-                &_ => {}
-            }
-        }
-        let output = Command::new(run_cmd)
-            .stdin(Stdio::from(test.input.unwrap()))
-            .output()
-            .expect("Failed to execute");
-        // Run the program
-        failures.push(path.to_str().unwrap().to_string());
-        match output {
-            Output { .. } if output.status.success() && test.exit_code != 0 => {
-                println!("  - Succeed on supposed fail");
-            }
-            Output { .. } if !output.status.success() && test.exit_code == 0 => {
-                println!("  - Fail on supposed Success");
-            }
-            Output { .. }
-                if std::str::from_utf8(output.stdout.as_slice()).unwrap().trim() != test.output.trim() =>
-            {
-                println!("  - Output not the same");
-                let input = fs::read_to_string(path.to_str().unwrap().to_owned() + "/in").unwrap();
-                println!(
-                    "\n```\ndiff:\n{}\n{}\n```\n",
-                    Colour::Green.paint(input),
-                    Colour::Red.paint(std::str::from_utf8(output.stdout.as_slice()).unwrap())
-                );
-            }
-            Output { .. } => {
-                failures.pop();
-                println!("  - {}", Colour::Green.paint("OK"))
-            }
-        }
+                Output { .. } => {
+                    " - ".to_string() + &Colour::Green.paint("OK").to_string()
+                }
+            };
+            println!(
+                "- {} [{}ms]:\n{}",
+                Colour::Cyan
+                    .bold()
+                    .paint(test_dir.file_name().to_str().unwrap()),
+                SystemTime::now().duration_since(time_start).unwrap().as_millis(),
+            out_string
+            );
+        }));
     }
-    if !failures.is_empty() {
-        println!(
-            "- {}:\n{:?}",
-            Colour::Red.bold().paint("Failures"),
-            failures
-        )
+    for i in handles {
+        i.join().unwrap();
     }
 }
