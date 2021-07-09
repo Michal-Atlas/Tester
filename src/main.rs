de1d91aaaa02bb8579f33560ed62fca2e7a156a7
Michal Atlas <michal.z.atlas@gmail.com>
2021-07-09 15:25:47 +0200

    Tried to rewrite mate into Tokio

--- src/main.rs
+++ src/main.rs
@@ -1,114 +1,98 @@
 use ansi_term::Colour;
-use std::env::args;
-use std::fs::{read_dir, File};
-use std::process::{Command, Output, Stdio};
+use std::ffi::{OsStr, OsString};
 use std::time::SystemTime;
-use std::{fs, thread};
+use tokio::{
+    fs::{read_dir, File},
+    io::Result,
+    process::Command,
+};
 
-#[derive(Debug)]
-struct Test {
-    input: Option<File>,
-    output: String,
-    exit_code: i32,
+#[tokio::main]
+async fn main() -> Result<()> {
+    let yaml = clap::load_yaml!("cli.yml");
+    let matches = clap::App::from_yaml(yaml).get_matches();
+
+    let test_dir = &matches.args["test_dir"].vals[0];
+    let run_cmd = &matches.args["run_cmd"].vals[0];
+
+    let mut tests = read_dir(test_dir).await?;
+
+    while let Some(d) = tests.next_entry().await? {
+        test(run_cmd, d).await?;
+    }
+
+    Ok(())
 }
 
-fn main() {
-    let mut args = args();
-    args.next(); // First is program name
-    let test_dir = &args.next().expect("No Test Path Provided");
-    let run_cmd = &args.next().expect("No Command To Run Provided");
-    let tests = match read_dir(test_dir) {
-        Ok(t) => t,
-        Err(e) => {
-            panic!("Problem reading directory: {}", e)
-        }
-    };
-    let mut handles = Vec::new();
-    for test_dir in tests {
-        let run_cmd = run_cmd.clone();
-        let time_start = SystemTime::now();
-        handles.push(thread::spawn(move || {
-            let test_dir = test_dir.unwrap();
-            let path = test_dir.path().clone();
-            let mut test = Test {
-                input: None,
-                output: "".to_string(),
-                exit_code: 0,
-            };
-            // Extract Test Info from Files
-            for f in path.read_dir().expect("Error reading TestDir") {
-                let f = match f {
-                    Ok(r) => r,
-                    Err(_) => {
-                        continue;
-                    }
-                };
-                match f.file_name().to_str().unwrap() {
-                    "in" => {
-                        test.input = Some(fs::File::open(f.path()).unwrap());
-                    }
-                    "out" => {
-                        test.output = fs::read_to_string(f.path()).unwrap();
-                    }
-                    "exit" => {
-                        test.exit_code = fs::read_to_string(f.path())
-                            .unwrap()
-                            .trim()
-                            .parse()
-                            .unwrap();
-                    }
-                    &_ => {}
-                }
-            }
-            let output = Command::new(run_cmd)
-                .stdin(Stdio::from(test.input.unwrap()))
-                .output()
-                .expect("Failed to execute");
-            // Run the program
+async fn test(run_cmd: &std::ffi::OsString, test_mod: tokio::fs::DirEntry) -> Result<()> {
+    let time_start = SystemTime::now();
+    println!("Ran test: {:?}", test_mod.path());
 
-            let out_string = match output {
-                Output { .. } if output.status.success() && test.exit_code != 0 => {
-                    "  - ".to_string() + &Colour::Red.paint("Succeed on supposed fail").to_string()
-                }
-                Output { .. } if !output.status.success() && test.exit_code == 0 => {
-                    "  - ".to_string() + &Colour::Red.paint("Fail on supposed Success").to_string()
-                }
-                Output { .. }
-                    if std::str::from_utf8(output.stdout.as_slice())
-                        .unwrap()
-                        .trim()
-                        != test.output.trim() =>
-                {
-                    "  - ".to_string()
-                        + &Colour::Red.paint("Output not the same\n\n").to_string()
-                        + "diff:\n"
-                        + &Colour::Green
-                            .paint(
-                                fs::read_to_string(path.to_str().unwrap().to_owned() + "/out")
-                                    .unwrap(),
-                            )
-                            .to_string()
-                        + "\n"
-                        + &Colour::Red
-                            .paint(std::str::from_utf8(output.stdout.as_slice()).unwrap())
-                            .to_string()
-                }
-                Output { .. } => " - ".to_string() + &Colour::Green.paint("OK").to_string(),
-            };
-            println!(
-                "- {} [{}ms]:\n{}",
-                Colour::Cyan
-                    .bold()
-                    .paint(test_dir.file_name().to_str().unwrap()),
-                SystemTime::now()
-                    .duration_since(time_start)
-                    .unwrap()
-                    .as_millis(),
-                out_string
-            );
-        }));
+    let mut dir = read_dir(test_mod.path()).await?;
+    let mut f_in = None;
+    let mut f_out = None;
+    let mut f_exit = None;
+    while let Some(f) = dir.next_entry().await? {
+        match f.file_name().to_str() {
+            Some("in") => {
+                f_in = Some(f);
+            }
+            Some("out") => {
+                f_out = Some(f);
+            }
+            Some("exit") => {
+                f_exit = Some(f);
+            }
+            _ => {}
+        }
     }
-    for i in handles {
-        i.join().unwrap();
-    }
+    let f_in = f_in.unwrap();
+    let f_out = tokio::fs::read_to_string(f_out.unwrap().path());
+    let f_exit = tokio::fs::read_to_string(f_exit.unwrap().path());
+
+    let output = tokio::process::Command::new(run_cmd)
+        .stdin(std::process::Stdio::from(std::fs::File::open(f_in.path())?))
+        .output()
+        .await?;
+
+    let f_exit = f_exit.await?;
+    let f_out = f_out.await?;
+    let out_string = match output {
+        _ if output.status.success() && f_exit.trim() != "0" => {
+            "  - ".to_string() + &Colour::Red.paint("Succeed on supposed fail").to_string()
+        }
+        _ if !output.status.success() && f_exit.trim() == "0" => {
+            "  - ".to_string() + &Colour::Red.paint("Fail on supposed Success").to_string()
+        }
+        _ if std::str::from_utf8(output.stdout.as_slice())
+            .unwrap()
+            .trim()
+            != f_out.trim() =>
+        {
+            " - ".to_string()
+                + &Colour::Red.paint("Output not the same\n\n").to_string()
+                + "diff:\n"
+                + &Colour::Green
+                    .paint(f_out)
+                    .to_string()
+                + "\n"
+                + &Colour::Red
+                    .paint(std::str::from_utf8(output.stdout.as_slice()).unwrap())
+                    .to_string()
+        }
+        _ => " - ".to_string() + &Colour::Green.paint("OK").to_string(),
+    };
+    println!(
+        "- {} [{}ms]:\n{}",
+        Colour::Cyan
+            .bold()
+            .paint(test_mod.file_name().to_str().unwrap()),
+        SystemTime::now()
+            .duration_since(time_start)
+            .unwrap()
+            .as_millis(),
+        out_string
+    );
+
+    Ok(())
 }
